da414382b5af1a68e57ca6f6a53968fe38c2039b
Kamaria DeRamus <krafixdesigns@gmail.com>
2019-03-21 11:21:04 -0400

    made changes to the NavBar, added a GamesComponent, will continue working on the Games UI

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-games',
+  selector: 'games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.css']
 })
