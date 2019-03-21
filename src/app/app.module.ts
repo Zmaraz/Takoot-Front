9aafa5b19445b4337d3a9feba48427ec4983bd75
Kamaria DeRamus <krafixdesigns@gmail.com>
2019-03-20 20:58:32 -0400

    added LoginComponent and navigateTo-Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 // import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+
+const ROUTES = [
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,6 +21,7 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     NgbModule,
+    RouterModule.forRoot(ROUTES)
     // FormsModule
   ],
   providers: [],
