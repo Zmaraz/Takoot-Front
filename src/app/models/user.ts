3896713ae0d0074614a5c60e7d7516c81d0a3caa
Kamaria DeRamus <krafixdesigns@gmail.com>
2019-03-27 19:44:29 -0400

    able to registergit add -Agit add -A!

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,14 +1,14 @@
 export class User {
     user_id: number;
-    first_name: string;
-    last_name: string;
+    firstName: string;
+    lastName: string;
     username: string;
     password: string;
     email: string
 
     constructor(firstname: string, lastname: string, username: string, password: string, email: string) {
-        this.first_name = firstname;
-        this.last_name = lastname;
+        this.firstName = firstname;
+        this.lastName = lastname;
         this.username = username;
         this.password = password;
         this.email = email;
