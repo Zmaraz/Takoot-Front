18e6bb15ad1ec65121b406da77a323949ca4434c
Mitchell-Elbus <elbus3@gmail.com>
2019-03-27 21:49:22 -0400

    trying to get profile page to display the user profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { User } from '../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+   // calling the method isAuthenticated$ from the auth service
+   isAuthenticated$ = this.authService.isAuthenticated$;
 
+   // creating variable, credentialsIsInvalid of type boolean
+   credentialsInvalid: boolean = false;
+ 
+   // credentials is a Credentials class/object
+   user: User;
+
+  constructor(private authService: AuthService, private router: Router) { 
+    console.log('Profileomponent constructed!');
+    this.showProfile;
+  }
+
+  showProfile(user: User) {
+    console.log('in showProfile()');
+    document.getElementById('usernameTag').innerText = this.user.username;
+    document.getElementById('firstnameTag').innerText = this.user.firstName;
+    document.getElementById('lastnameTag').innerText = this.user.lastName;
+    document.getElementById('passwordTag').innerText = this.user.password;
+    document.getElementById('emailTag').innerText = this.user.email;
+  }
+
+  
   ngOnInit() {
+    this.showProfile;
   }
 
 }
