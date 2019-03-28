3549b46f5d840bbec48629906a69c27032ec4814
Alex Johnson <alexjohnson4564@gmail.com>
2019-03-27 21:45:00 -0400

    edited the questions to generate 5 questions instead of 10

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { QuestionService } from '../question.service';
 import { Quiz } from '../models/quiz';
+import { Question } from '../models/question';
 
 @Component({
   selector: 'questions',
@@ -10,18 +11,21 @@ import { Quiz } from '../models/quiz';
 })
 export class QuestionsComponent implements OnInit {
 
+  // this is for generating a question from the API
+  // these need to remain separate!!!!
   question: Object;
-  theQuiz: Quiz;
 
-  // calling the generateQuestion from the DataService
+  // these are for creating a quiz
+  theQuiz: Quiz;
+  theQuestion: Question;
+  theAnswer: Object;
 
   constructor(private results: DataService, private questions:QuestionService) { }
 
   ngOnInit() {
-    // generate a question should work here
-    // this.results.generateQuestion;
   }
 
+  // calls the API
   getQuestion() {
     this.results.generateQuestion().subscribe(results => {
       this.question = results;
@@ -29,6 +33,7 @@ export class QuestionsComponent implements OnInit {
     });
   }
 
+  // creates a Quiz
   createQuiz(title, dateCreated, dateLastUpdated, categoryId, difficultyId, defaultId) {
 
     // this part of the function, sends the Quiz
@@ -42,11 +47,23 @@ export class QuestionsComponent implements OnInit {
     this.theQuiz = new Quiz (title, dateCreated, dateLastUpdated, categoryId, difficultyId, defaultId);
     console.log(this.theQuiz);
 
-
-    // this part of the function, sends all the questions
-
     this.questions.create(this.theQuiz);
+    
   }
 
+  // creates a Question
+  createQuestion1(quest) {
+    quest = (<HTMLInputElement>document.getElementById('quest1')).value;
+    this.theQuestion = new Question(quest);
+    console.log(this.theQuestion);
+    this.questions.createQuest(this.theQuestion);
+  }
+
+  createQuestion2(quest) {
+    quest = (<HTMLInputElement>document.getElementById('quest2')).value;
+    this.theQuestion = new Question (quest);
+    console.log(this.theQuestion);
+    this.questions.createQuest(this.theQuestion);
+  }
 
 }
