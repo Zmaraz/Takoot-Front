5605e989ceff69856caf7b225654da74e050aa4a
Alex Johnson <alexjohnson4564@gmail.com>
2019-03-27 20:48:56 -0400

    semi-working create Quiz; will send info to database

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -11,7 +11,7 @@ import { Quiz } from '../models/quiz';
 export class QuestionsComponent implements OnInit {
 
   question: Object;
-  theQuiz: Object;
+  theQuiz: Quiz;
 
   // calling the generateQuestion from the DataService
 
@@ -35,18 +35,19 @@ export class QuestionsComponent implements OnInit {
     dateCreated = new Date();
     dateLastUpdated = dateCreated;
 
+    // var opt = sel.options[sel.selectedIndex];
     // will come back to parse through and get the value that user clicked
-    categoryId = document.getElementById('');
+    categoryId = (<HTMLInputElement>document.getElementById('category')).value;
 
     // will come back to parse through and get the value that user clicked
-    difficultyId = document.getElementById('');
+    difficultyId = (<HTMLInputElement>document.getElementById('difficulty')).value;
 
     defaultId = 2;
 
     this.theQuiz = new Quiz (title, dateCreated, dateLastUpdated, categoryId, difficultyId, defaultId);
     console.log(this.theQuiz);
 
-    // this.questions.create(this.theQuiz);
+    this.questions.create(this.theQuiz);
   }
 
 
